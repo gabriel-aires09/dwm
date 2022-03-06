a3a1cbdbcddccf5d5f9b121e738ba4f52bd56e68
Gabriel Oliveira Aires <90778217+gabriel-aires09@users.noreply.github.com>
2022-03-05 23:42:35 -0300

    Mudança de espaçamento entre linhas 85 e 86

--- config.h
+++ config.h
@@ -82,8 +82,8 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,							XK_l,			 spawn,					 {.v = lockcmd } },
-	{ MODKEY|ShiftMask,							XK_p,	 		 spawn,					 {.v = screencmd } },
+	{ MODKEY|ShiftMask,		XK_l,	   spawn,	   {.v = lockcmd } },
+	{ MODKEY|ShiftMask,		XK_p,	   spawn,	   {.v = screencmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
